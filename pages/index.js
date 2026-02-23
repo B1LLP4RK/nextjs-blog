128a4cbcd779734742e0c49c8a0d6a96f205ff54
junha park <bd08001@gmail.com>
2026-02-23 19:43:18 +0800

    fix introduction

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>hello I am junha park</p>
-        <p>
-          I am a computer science student in NUS
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>I am a computer science student in NUS</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
